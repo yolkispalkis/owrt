6391ad23becb7b15bb531c385a002b9132000fca
Maximilian Weinmann <x1@disroot.org>
2021-11-22 22:10:20 +0700

    ramips: Add support for Sercomm S1500 (Beeline SmartBox PRO, WiFire Sercomm S1500.NBN)
    Signed-off-by: Maximilian Weinmann <x1@disroot.org>

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -55,6 +55,8 @@ platform_do_upgrade() {
 	asus,rt-ac85p|\
 	beeline,smartbox-flash|\
 	beeline,smartbox-giga|\
+	beeline,smartbox-pro|\
+	wifire,s1500-nbn|\
 	dlink,dir-1960-a1|\
 	dlink,dir-2640-a1|\
 	dlink,dir-2660-a1|\
